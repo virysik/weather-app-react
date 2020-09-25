0941db0c683a9dbdf5bc5f36d65f8e5320474501
virysik <virysik@gmail.com>
2020-09-25 13:47:16 +0200

    Added loader hearts

---
+++ src/Weather.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Loader from "react-loader-spinner";
+
+export default function Weather() {
+  return <Loader type="Hearts" color="#00BFFF" height={80} width={80} />;
+}
